44cd1aed75341f17a2092bb428dddd77135e14ad
Sidhant Nagpal <sidnagpal2000@gmail.com>
2023-03-05 13:02:53 +0530

    updated checkout after offers

--- src/main/scala/ShoppingCart.scala
+++ src/main/scala/ShoppingCart.scala
@@ -9,9 +9,15 @@ class ShoppingCart {
   val appleCost: Int = 60
   val orangeCost: Int = 25
 
+  def appleCount(): Int = cart.count(_.toLowerCase() == "apple")
+
+  def orangeCount(): Int = cart.count(_.toLowerCase() == "orange")
+
+  private def discount: Int = (this.appleCount / 2) * 60 + (this.orangeCount / 3) * 25
+
   @tailrec
   final def totalCost(accumulator: Int = 0, n: Int = 0): Float = {
-    if (n >= cart.length) accumulator
+    if (n >= cart.length) accumulator - discount
     else if (cart(n).toLowerCase() == "apple") totalCost(accumulator + appleCost, n + 1)
     else totalCost(accumulator + orangeCost, n + 1)
   }
