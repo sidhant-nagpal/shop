6d2ac8c6fc260e97c84325e67f60f617db383761
Sidhant Nagpal <sidnagpal2000@gmail.com>
2023-03-05 13:40:52 +0530

    added tests

--- src/test/scala/TestingShoppingCart.scala
+++ src/test/scala/TestingShoppingCart.scala
@@ -1,18 +1,17 @@
 import org.scalatest.funspec.AnyFunSpec
 
 class TestingShoppingCart extends AnyFunSpec {
-
   val cart = new ShoppingCart(List("Apple", "Apple", "Orange", "Orange", "Orange"));
   val demoItems: List[String] = List("Apple", "Apple", "Orange", "Orange", "Orange")
 
-  describe("ShoppingCart"){
+  describe("ShoppingCart") {
 
-    it("should count the items correctly"){
+    it("should count the items correctly") {
       assert(cart.count == demoItems.length)
     }
 
     val appleCount: Int = demoItems.count(_.toLowerCase() == "apple")
-    it("should be able to count apples"){
+    it("should be able to count apples") {
       assert(cart.appleCount() == appleCount)
     }
 
@@ -24,14 +23,15 @@ class TestingShoppingCart extends AnyFunSpec {
 
     val appleOffer = (appleCount / 2) * 60
     val orangeOffer = (orangeCount / 3) * 25
-    it("should calculate the total discount amount correctly"){
+    it("should calculate the total discount amount correctly") {
       assert(cart.discount == appleOffer + orangeOffer)
     }
 
-    it("should calculate the total cost after discount at checkout"){
-      val totalCost = appleCount*60 + orangeCount*25
-      assert(cart.totalCost() == totalCost - (appleOffer+orangeOffer))
+    it("should calculate the total cost after discount at checkout") {
+      val totalCost = appleCount * 60 + orangeCount * 25
+      assert(cart.totalCost() == totalCost - (appleOffer + orangeOffer))
     }
+
   }
 }
 
